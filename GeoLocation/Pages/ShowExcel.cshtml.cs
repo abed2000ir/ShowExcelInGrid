19623c487835694314f6474c37a0e04006345b01
abed2000ir <abed2000ir@gmail.com>
2023-06-17 12:13:15 +0330

    Add link to get Geolocation

--- GeoLocation/Pages/ShowExcel.cshtml.cs
+++ GeoLocation/Pages/ShowExcel.cshtml.cs
@@ -3,6 +3,7 @@ using GeoLocation.Utilities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using OfficeOpenXml;
+using System.IO;
 
 namespace GeoLocation.Pages
 {
@@ -18,6 +19,7 @@ namespace GeoLocation.Pages
         {
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
             Environment = _enviroment;
+
         }
 
         public void OnGet()
@@ -49,8 +51,10 @@ namespace GeoLocation.Pages
             RedirectToPage();
 
         }
-        public void OnGetFirstData()
+        public void OnGetFirstData(string filename)
         {
+            ViewData["Sended_File"] = filename;
+
             Current = 20;
             GridData = LsExcelData.Take(20).ToList();
             RedirectToPage();
@@ -87,22 +91,31 @@ namespace GeoLocation.Pages
                 RedirectToPage("./Error", "ShowError", new { message = ex.Message });
             }
         }
+
+        private IList<ExcelData> GetExcelContent(string FileFullpath)
+        {
+            using (var package = new ExcelPackage(new FileInfo(Path.Combine(FileFullpath))))
+            {
+                ExcelWorksheet sheet = package.Workbook.Worksheets[0];
+                return sheet.ConvertSheetToObjects<ExcelData>().ToList();
+            }
+
+        }
+
         public IActionResult OnGetProcessExcel([System.Web.Http.FromUri] string request)
         {
             try
             {
+                ViewData["Sended_File"] = request;
                 string path = Path.Combine(this.Environment.WebRootPath, "Upload");
 
                 if (new string[] { ".xlsx", ".csv" }.Any(new FileInfo(Path.Combine(path, request)).Extension.Equals))
                 {
-                    using (var package = new ExcelPackage(new FileInfo(Path.Combine(path, request))))
-                    {
-                        ExcelWorksheet sheet = package.Workbook.Worksheets[0];
-                        LsExcelData = sheet.ConvertSheetToObjects<ExcelData>().ToList();
-                    }
+
+                    LsExcelData = GetExcelContent(Path.Combine(path, request));
                     Current = 20;
                     GridData = LsExcelData.Take(20).ToList();
-                    return RedirectToPage("./ShowExcel", "FirstData");// RedirectToPage("./ShowExcel");
+                    return RedirectToPage("./ShowExcel", "FirstData", new { filename = request });// RedirectToPage("./ShowExcel");
                 }
                 else
                 {
@@ -114,5 +127,71 @@ namespace GeoLocation.Pages
                 return RedirectToPage("./Error", "ShowError", new { message = ex.Message });
             }
         }
+
+
+        public async Task<IActionResult> OnPostGetLocations(string filename)
+        {
+            try
+            {
+                string path = Path.Combine(this.Environment.WebRootPath, "Upload");
+
+                LsExcelData = GetExcelContent(Path.Combine(path, filename));
+                // LsExcelData = LsExcelData.OrderBy(c => c.Postalcode).ToList();
+
+                HttpClient client = new HttpClient();
+                client.Timeout = new TimeSpan(0, 5, 0);
+                var code = "";
+                var responseBody = "";
+
+                if (new FileInfo(Path.Combine(path, filename) + ".log").Exists)
+                {
+                    IList<FileInfo> lsFile = new DirectoryInfo(Path.Combine(path)).GetFiles("*_Geo*.*.log").OrderBy(c => c.Name).ToList();
+
+                    if (lsFile.Count > 0)
+                    {
+                        filename = lsFile[lsFile.Count - 1].Name.Substring(0, lsFile[lsFile.Count - 1].Name.LastIndexOf(".log"));//.Replace(".log","");
+
+                        filename = filename.Split("_Geo").First() + "_Geo" + (int.Parse(filename.Split('.').First().Split("_Geo").Last()) + 1) + "." + filename.Split('.').Last();
+                    }
+                    else
+                        filename = filename.Split('.').First() + "_Geo1." + filename.Split('.').Last();
+
+                }
+
+                foreach (var item in LsExcelData)
+                {
+                    code = item.Postalcode;
+
+                    try
+                    {
+                        using HttpResponseMessage response = await client.GetAsync("******" + code);
+                        response.EnsureSuccessStatusCode();
+                        responseBody = response.Content.ReadAsStringAsync().Result;
+                    }
+                    catch
+                    {
+                        responseBody = "";
+                    }
+
+
+                    using (System.IO.StreamWriter sw = new StreamWriter(Path.Combine(path, filename) + ".log", true))
+                    {
+                        sw.WriteLine($"{code},{responseBody}");
+                    }
+
+                }
+
+                return RedirectToPage("./ShowExcel", "FirstData");// RedirectToPage("./ShowExcel");
+
+            }
+            catch (Exception ex)
+            {
+
+                return RedirectToPage("./Error", "ShowError", new { message = ex.Message });
+
+            }
+
+        }
+
     }
 }
